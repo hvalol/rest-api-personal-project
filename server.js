c841b7ba3efee7584530ec37f65df38c573fd8c2
hurleyvalen <hurleyvalen.oasislogic@gmail.com>
2025-06-05 16:31:27 +0800

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const publicApiRoutes = require("./routes/publicApi");
 const app = express();
 
 // define port
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 // enable cors for all routes for development
